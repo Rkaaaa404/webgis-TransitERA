5b410b5447867233dc8f4f4c32e231d3850705a4
Rkaaaa404 <raykapranandita@gmail.com>
2026-09-03 10:33:48 +0700

    fix(frontend): enable output standalone in next.config.ts for docker build

--- webdev/frontend/next.config.ts
+++ webdev/frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   // Allow access from local network devices
   allowedDevOrigins: ['192.168.1.16', 'http://192.168.1.16:3000', '192.168.1.17', 'http://192.168.1.17:3000', 'localhost:3000', 'http://localhost:3000'],
 };
